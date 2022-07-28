45b5a80ee84f1cc235e5f3e1f0348ec58d90d9c9
Adarsh Kumar <2020kucp1016@iiitkota.ac.in>
2022-07-28 20:40:59 +0530

    msg

--- src/index.js
+++ src/index.js
@@ -2,7 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import * as firebase from 'firebase';
+import 'firebase/firestore';
 
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAqNx9KgVdukJF9cJioiGaAHS_2Mi0er2A",
+  authDomain: "cart-ab8c3.firebaseapp.com",
+  projectId: "cart-ab8c3",
+  storageBucket: "cart-ab8c3.appspot.com",
+  messagingSenderId: "830028316388",
+  appId: "1:830028316388:web:8767567f782bef0002f6f1"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
